bd8a05ae66c84ff74bc047cf58f5b6229a3e3967
xiuyuma <ma79@wisc.edu>
2018-02-05 15:51:01 -0600

    update random dist

--- scDDboost_pkg/R/EBS_D_random.R
+++ scDDboost_pkg/R/EBS_D_random.R
@@ -13,15 +13,17 @@
 #' @return new distance matrix
 
 EBS_D_random = function(data, K, gcl, sz, D, hp, Posp, iter, lambda){
-    E=rexp(ncol(D),rate = lambda)
+    for(i in 1:iter){
+    E = rexp(ncol(D),rate = lambda)
     n = ncol(D)
-    d_mean = mean(D) * n / (n-1)
-    weights=d_mean*E/2
-    R_D = sapply(1:ncol(D), function(i) sapply(1:ncol(D), function(j) {if(i != j){return(D[i,j]+weights[i]+weights[j])}else{return(0)}}))
+    d_mean = sd(D)
+    weights=d_mean * E / 2
+    R_D = sapply(1:n, function(i) sapply(1:ncol(D), function(j) {if(i != j){return(D[i,j]+weights[i]+weights[j])}else{return(0)}}))
     ccl = pam(R_D, k = K, diss = T)$clustering
     res = EBS(data,ccl,gcl,sz,iter,hp,Posp)
     DE = res$DEpattern
     Y = apply(DE, 1, which.max)
-    Ebs_D = EBS_D(ccl, Posp, Y)
-    return(Ebs_D)
+    Ebs_D = Ebs_D + EBS_D(ccl, Posp, Y)
+    }
+    return(Ebs_D/iter)
 }
