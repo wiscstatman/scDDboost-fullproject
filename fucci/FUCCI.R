b1ca6e2a8ac8e2a508e55fe67279d151738ca069
xiuyuma <ma79@wisc.edu>
2018-07-22 16:05:31 -0400

    really hard to beat scDD

--- fucci/FUCCI.R
+++ fucci/FUCCI.R
@@ -8,32 +8,47 @@ library(SingleCellExperiment)
 cd = rep(1,247) ##treat all cells to be in one condition for normalization
 DataNorm<-SCnorm(Data=data_count,Conditions=cd,PrintProgressPlots=F,FilterCellNum=10,NCores = 4)
 data_all = assays(DataNorm)$Counts
-
+data_all = round(edat)
+data_all = as.matrix(data_all)
 ##generate distance matrix
 D_all = cal_D(data_all, 10)
-##8 subtypes
-K = 8
+##7 subtypes
+K = 9
 Posp = pat(K)[[1]] #possible partition for 7 groups
 
+cd = c(rep(1,91),rep(2,80))
+##generate refinement relation between partitions
+ref[[K]] = g_ref(Posp)
+ref = list()
+for(i in 2:8){
+  ref[[i]] = g_ref(pat(i)[[1]])
+}
 
 ##first only consider DD genes between G1 vs G2
-ccl = pam(D_all,K, T)$clustering[1:167]  ##get clustering
-data_count = data_all[,1:167]
-D_c = D_all[1:167,1:167]
+ccl = pam(D_all,K, T)$clustering[c(1:91,168:247)]  ##get clustering
+#ccl = pam(D_c,K,T)$clustering
+G1 = data_all[,1:91]
+G2 = data_all[,92:167]
+S = data_all[,168:247]
+data_count = cbind(G1,S)
+D_c = D_all[c(1:91,168:247),c(1:91,168:247)]
 ccl ##can view the different composition of subtypes between G1 and G2
 sz = MedianNorm(data_count)
+#sz = rep(1,ncol(data_count))
 hp = c(1, rep(1,nrow(data_count))) ##get hyper parameter
-pDD7 = PDD(data = data_count, cd = cd, ncores = 1, K = K, D = D_c,
-           sz = sz, hp, pat(K)[[1]], 10, random = T, lambda = 1, nrandom = 100)
+pDD7= PDD(data = data_count, cd = cd, ncores = 1, K = K, D = D_c,
+           sz = sz, hp, pat(K)[[1]], 10, random = T, lambda = 1, nrandom = 10)
 EDDb = which(pDD7 > 0.95)
-length(EDDb) ##having 252 DD genes
+length(EDDb)
 
-pDD7_no_random = PDD(data = data_count, cd = cd, ncores = 1, K = K, D = D_c,
+pDD9_no_random = PDD(data = data_count, cd = cd, ncores = 1, K = K, D = D_c,
                      sz = sz, hp, pat(K)[[1]], 10, random = F, lambda = 1, nrandom = 0)
-length(which(pDD7_no_random > 0.95))
+length(which(pDD9_no_random > 0.99))
+EDD9 = which(pDD9_no_random > 0.99)
+gcl = 1:nrow(data_count)
 ##having 1352 DD genes without randomization by set
-
-
+ebres = EBS(data_count,ccl,gcl,sz,10,hp,Posp)
+DE = ebres$DEpattern
 
 ##MAST
 library(MAST)
@@ -58,19 +73,65 @@ fcHurdle <- merge(summaryDt[contrast=='condition2' & component=='H',.(primerid, 
                   summaryDt[contrast=='condition2' & component=='logFC', .(primerid, coef, ci.hi, ci.lo)], by='primerid') 
 
 fcHurdle[,fdr:=p.adjust(`Pr(>Chisq)`, 'fdr')]
-fcHurdleSig <- merge(fcHurdle[fdr<.05 & abs(coef)> 0], as.data.table(mcols(MNZ10)), by='primerid')
+fcHurdleSig <- merge(fcHurdle[fdr<.01 & abs(coef)> 0], as.data.table(mcols(MNZ10)), by='primerid')
 
 EDDM = as.numeric(fcHurdleSig$gene)
-
+length(EDDM)
 ##MAST has 504 DD genes
 prior_param=list(alpha=0.01, mu0=0, s0=0.01, a0=0.01, b0=0.01)
 condition = factor(cd)
 X =  SingleCellExperiment(assays = list(normcounts = data_count), 
                           colData = data.frame(condition))
 
-X_scDD <- scDD(X, prior_param=prior_param, testZeroes=T)
+X_scDD <- scDD(X, prior_param=prior_param, testZeroes=T, permutations = F)
 RES = scDD::results(X_scDD)
-EDD_dz = which(RES$nonzero.pvalue.adj < 0.05)
+EDD_dz = which(RES$nonzero.pvalue.adj < 0.01)
+EDD_sc = which(RES$combined.pvalue < 0.01)
 #DDc = RES$DDcategory
 #table(DDc)
 ##scDD has 1575 DD genes. 
+un1 = union(EDDM,EDD_sc)
+scb_uni = setdiff(EDD9, un1)
+
+tran = list()
+J = 6
+tmpp = sample(EDD9,J)
+tmp = scb_uni[1:J]
+for(i in 1:J){
+  tran[[i]] = data_count[tmp,]
+}
+
+
+cur_rn = rn[tmp]
+
+cond_ind = c(rep("1", length(which(cd ==1))),
+             rep("2", length(which(cd==2))))
+
+cur_rn = factor(cur_rn, levels = cur_rn)
+df = data.frame(x = rep(cond_ind, J), y = log(do.call(c, tran) + 1), z = rep(cur_rn, each = length(cd)))
+
+
+#pdf("density_G48_dd.pdf")
+
+pp<-ggplot(df,aes(factor(x),y))+ geom_violin(aes(colour = factor(x))) + geom_point(size = 0.1,position = position_jitter(w = 0.05, h = 0)) +
+  xlab("conditions") + 
+  ylab("gene expressions")+
+  theme(panel.background = element_rect(
+    fill = 'white', colour = 'black'),
+    axis.title.x=element_blank(),
+    axis.text.x=element_blank(),
+    axis.title.y=element_blank(),
+    legend.title=element_blank(),
+    panel.grid.minor.x = element_line(size = 0.5),
+    panel.grid.minor.y = element_line(size = 0.5),
+    panel.grid.major.x = element_line(size = 0.5),
+    panel.grid.major.y = element_line(size = 0.5),
+    panel.grid.major = element_line(colour = "grey"))
+pp + facet_wrap( ~ z, ncol = 2)
+#dev.off()
+
+
+
+
+
+
