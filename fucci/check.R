e2cc70444b5e9373160274b788ed09e04b2c21a9
Michael Newton <wiscstatman@gmail.com>
2018-07-24 09:36:42 -0500

    more on fucci

---
+++ fucci/check.R
@@ -0,0 +1,18 @@
+
+source("https://bioconductor.org/biocLite.R")
+biocLite("SingleCellExperiment")
+biocLite("SummarizedExperiment")
+biocLite("MAST")
+biocLite("scDD")
+
+# install(data.table)
+install("/Users/michaelnewton/wip/scDDboost/scDDboost/scDDboost_pkg")
+
+
+library(SingleCellExperiment)
+library(SummarizedExperiment)
+library(scDDboost)
+library(MAST)
+library(scDD)
+library(SingleCellExperiment)
+library(data.table)
