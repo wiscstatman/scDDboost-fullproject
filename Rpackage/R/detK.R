853fe0e7e7f57b0ee17812ca1998b25f01bca552
xiuyuma <ma79@wisc.edu>
2019-03-07 20:19:53 -0600

    pam should indicate diss = T

--- Rpackage/R/detK.R
+++ Rpackage/R/detK.R
@@ -4,7 +4,7 @@ detK = function(D,epi = 0.15)
      inter = rep(0,8)
     
      for(i in 2:9){
-         clusRes = pam(D,i)
+         clusRes = pam(D,i,diss = T)
          intra[i - 1] = as.numeric(clusRes$objective[1])
          x = clusRes$id.med
          inter[i - 1] = sum(D[x,x]) / (i * (i - 1))
