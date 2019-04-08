153075cc65d92f88010d657b9d4e18405dff21dc
Michael Newton <wiscstatman@gmail.com>
2019-04-08 09:14:14 -0500

    plus wilcoxon

--- stats/fucci/bh.R
+++ stats/fucci/bh.R
@@ -11,16 +11,21 @@ tdat <- log2( data_count + 1/2 )
 X <- tdat[,cd==1]
 Y <- tdat[,cd==2]
 
-pv <- rep(1,nrow(X))
+pv.t <- rep(1,nrow(X))
+pv.w <- rep(1,nrow(X))
 
 for( i in 1:nrow(X) )
  {
   if( var( c(X[i,], Y[i,] ) ) > 0 )
    {
-     pv[i]  <- ( t.test( X[i,], Y[i,] ) )$p.value
+     pv.t[i]  <- ( t.test( X[i,], Y[i,] ) )$p.value
+     pv.w[i]  <- ( wilcox.test( X[i,], Y[i,] ) )$p.value
    }
  }
 
-qv <- p.adjust( pv, method="BH" )  ## looks like a lot 
+qv.t <- p.adjust( pv.t, method="BH" )  ## looks like a lot 
+qv.w <- p.adjust( pv.w, method="BH" )  ## l
 
-sum( qv <= 0.05 )  ## I find 3361...not sure how this compares to other methods
+sum( qv.t <= 0.05 )  ## I find 3361...not sure how this compares to other methods
+sum( qv.w <= 0.05 )  ## I find 3496 ...not sure how this compares to other methods
+
