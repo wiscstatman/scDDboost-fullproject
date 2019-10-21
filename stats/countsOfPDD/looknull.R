295dd4c042da3172172cc20e193ef617f2664f07
xiuyuma <ma79@wisc.edu>
2019-10-21 00:38:57 -0500

    clean

---
+++ stats/countsOfPDD/looknull.R
@@ -0,0 +1,19 @@
+
+ss <- read.csv("null_0.95.csv", row.names=1)
+
+tt <- ss[-1,1:3]  ## dropping one extreme DD case
+
+oo <- order( -tt[,1] )
+oo <- order( -rowSums(tt) )
+#oo <- order( -apply(tt,1,var) )
+###oo <- c(6,4,5, 3,7, 8,2,1 )
+qq <- tt[oo,]
+
+pdf( file="null95.pdf", height=5, width=8 )
+cls <- c("blue", "green", "darkorange" )
+matplot( qq, type="b", axes=FALSE, xlab="",ylab="", col=cls, lwd=4,
+	 lty=c(5,2,1), cex=2, ylim=c(0,100)  )
+axis( side=1, at=1:7, labels=rownames(qq), cex.axis=.85 )
+axis( side=2, las=1, cex.axis=.9 )
+box(bty="l")
+dev.off()
