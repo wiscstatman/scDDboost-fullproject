ce745aef12806df304de8e9d84ac972d49d7d5a7
xiuyuma <ma79@wisc.edu>
2020-07-23 19:47:37 -0400

    updates

--- 1-revision/exp/ks.R
+++ 1-revision/exp/ks.R
@@ -28,10 +28,54 @@ k_pval = p.adjust(kpval,"fdr")
 
 
 
+### data (Fucci)
+
+load("Fucci-Data.RData")
+
+## data_counts: the expression matrix
+## cd: condition label
+
+## genenames: gene names
+## cell_cycle_genes: name for cell cycle related genes
+
+## p_scDDboost: probabilities of DD by scDDboost
+## p_scDD, p_mast, p_deseq2, p_t, p_ks: fdr adjusted p values by scDD, MAST, DESeq2, t-test, ks-test.
+
+## get DD genes with 0.05 threshold
+scDDb_dd = lsz(p_scDDboost,0.05)
+
+## get the corresponding gene names
+scDDb_genes = genenames[scDDb_dd]
 
 
+## similar for MAST, scDD, DESeq2, t-test and ks-test
+mst_dd = which(p_mast < 0.05)
+scdd_dd = which(p_scDD < 0.05)
+des_dd = which(p_deseq2 < 0.05)
+t_dd = which(p_t < 0.05)
+ks_dd = which(p_ks < 0.05)
 
+mst_genes = genenames[mst_dd]
+scdd_genes = genenames[scdd_dd]
+des_genes = genenames[des_dd]
+t_genes = genenames[t_dd]
+ks_genes = genenames[ks_dd]
 
+## union of DD genes by MAST,scDD,DESeq2 and t-test
+U_genes = union(union(union(mst_genes,scdd_genes),des_genes),t_genes)
 
+## uniquely by scDDboost
+uni_genes = setdiff(scDDb_genes, U_genes)
 
+## intersection with cell cycle related, 137 uniquely found by scDDboost
+sum(scDDb_genes %in% cell_cycle_genes)
 
+## only 2 genes found by k_genes also in those uni_genes by scDDboost
+
+sum(k_genes %in% intersect(uni_genes,cell_cycle_genes))
+
+## which are
+
+k_genes[which(k_genes %in% intersect(uni_genes,cell_cycle_genes) == T)]
+
+## they are not the 3 genes we found support for DD in G1 vs G2/M
